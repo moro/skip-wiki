12f1576b02cba75ee9d14795b988df335f474f88
moro <moro@1a9bd5c6-a57e-11dd-8275-8af11251ec36>
2008-11-28 02:47:49 +0000

    jsonでのレスポンスを実装
    git-svn-id: http://svn.openskip.org/labo/skip-note/trunk@61 1a9bd5c6-a57e-11dd-8275-8af11251ec36

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -8,7 +8,9 @@ class AttachmentsController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.js
+      format.js do
+        render :json => @attachments.map{|a| {:attachment=>attachment_to_json(a)} }
+      end
     end
   end
 
@@ -28,18 +30,27 @@ class AttachmentsController < ApplicationController
   end
 
   private
+  def attachment_to_json(atmt)
+    returning(atmt.attributes.slice("display_name", "created_at", "updated_at")) do |json|
+      json[:path] = target_attachment_path(atmt)
+      json[:inline] = target_attachment_path(atmt, :position=>"inline") if atmt.image?
+    end
+  end
+
   def current_target
     @_current_target ||= \
       params[:page_id] ? current_note.pages.find(params[:page_id]) : current_note
   end
 
+  def target_attachments_path;   URI(target_attachments_url).path end
   def target_attachments_url
     params[:page_id] ? note_page_attachments_url(current_note, current_target) \
                      : note_attachments_url(current_target)
   end
 
-  def target_attachment_url(at)
-    at.attachable_type == "Page" ? note_page_attachment_url(current_note, current_target, at) \
-                                 : note_attachment_url(current_target, at)
+  def target_attachment_path(a, opts = {}); URI(target_attachment_url(a, opts)).path end
+  def target_attachment_url(at, opts = {})
+    at.attachable_type == "Page" ? note_page_attachment_url(current_note, current_target, at, opts) \
+                                 : note_attachment_url(current_target, at, opts)
   end
 end
---
+++ spec/controllers/attachment_controller_spec.rb
@@ -0,0 +1,46 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe AttachmentsController do
+  fixtures :users, :notes
+  before do
+    controller.stub!(:current_user).and_return(@user = users(:quentin))
+    controller.stub!(:current_note).and_return(@note = notes(:our_note))
+  end
+
+  describe "GET index.js" do
+    before do
+      @attachments = [
+        @note.attachments.create!(:uploaded_data => fixture_file_upload("data/at_small.png", "image/png", true),
+                                  :display_name  => "user iconとかの画像です"),
+      ]
+      xhr :get, :index, :note_id=>notes(:our_note)
+    end
+
+    it do
+      response.should be_success
+    end
+
+    describe "レスポンスのJSON" do
+      before do
+        @attachments_json = ActiveSupport::JSON.decode(response.body)
+      end
+
+      it do
+        @attachments_json.should be_an_instance_of(Array)
+      end
+
+      it "最初のデータの[attachment][display_name]は/^user icon/にマッチすること" do
+        @attachments_json.first["attachment"]["display_name"].should =~ /^user icon/
+      end
+
+      it "最初のデータの[attachment][path]は%r[/notes/our_note/attachments/\d+]にマッチすること" do
+        @attachments_json.first["attachment"]["path"].should =~ %r[/notes/our_note/attachments/\d+]
+      end
+
+      it "最初のデータの[attachment][inline]がnilでないこと" do
+        @attachments_json.first["attachment"]["inline"].should_not be_nil
+      end
+    end
+  end
+end
+
