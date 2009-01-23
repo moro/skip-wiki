d019623a890a10cc9fceca82fc823182c182836e
moro <moronatural@gmail.com>
2009-01-23 09:37:12 +0900

    自動アップロード時にインジケータを表示させるようにした

--- public/javascripts/iframe_upload.js
+++ public/javascripts/iframe_upload.js
@@ -8,7 +8,8 @@
         var file = f.find("input[type=file]");
         var name = f.find("input[type=text]");
         if(file.val().length > 0 && name.val().length > 0){
-          submit = function(){ f.submit(); f.get(0).reset(); timer = null};
+          var indicator = f.find("td.indicator img").show();
+          submit = function(){ f.submit(); f.get(0).reset(); indicator.hide(); name.focus(); timer = null};
           timer = setTimeout(submit, 5*1000);
         }else{
           return;
