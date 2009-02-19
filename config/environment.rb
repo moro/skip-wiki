034f2140654845f530eee2a6f62cd97092981886
moro <moronatural@gmail.com>
2009-02-19 09:38:45 +0900

    use public version of scope_do gem.

--- config/environment.rb
+++ config/environment.rb
@@ -29,7 +29,7 @@ Rails::Initializer.run do |config|
   config.gem 'gettext',  :lib => 'gettext/rails', :version => '1.93.0'
   config.gem 'diff-lcs', :lib => 'diff/lcs'
   config.gem 'haml'
-  config.gem 'scope_do'
+  config.gem 'moro-scope_do', :lib => 'scope_do', :version => '>=0.1.1'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version=> '>=2.3.6', :source => "http://gems.github.com/"
   config.gem 'cucumber' if Rails.env == "test"
 
@@ -40,11 +40,11 @@ Rails::Initializer.run do |config|
   config.plugins = %w[
     friendly_id
     haml
-    scope_do
     jrails
     restful_authentication
     open_id_authentication
     attachment_fu
+    moro-scope_do
   ]
   config.plugins += %w[rails-footnotes] if Rails.env == "development"
   config.plugins += %w[webrat] if Rails.env == "test"
