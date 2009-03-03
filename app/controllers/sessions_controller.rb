cff8c1dda837107f446af96bdb468226975fdf22
moro <moronatural@gmail.com>
2009-03-03 22:19:05 +0900

    refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # This controller handles the login/logout function of the site.
 class SessionsController < ApplicationController
   before_filter :login_required, :except=>%w[new create]
+  before_filter :specified_op_is_accesptable, :only => %w[create]
 
   module AxHandler
     def self.included(base)
@@ -28,15 +29,30 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def create
-    logout_keeping_session!
-
+  def specified_op_is_accesptable
     if params[:openid_url] && !SkipCollabo::OpFixation.accept?(params[:openid_url])
       logger.debug("login refused since #{params[:openid_url]} is not member #{SkipCollabo::OpFixation.servers.inspect}")
       login_failed(params.merge(:openid_url=>params[:openid_url]))
     else
-      login_with_openid(params[:openid_url])
+      true
     end
+  end
+
+  def create
+    logout_keeping_session!
+    options = { :optional => self.class.ax_attributes.values.flatten,
+                :method   => "get" }
+
+    authenticate_with_open_id(params[:openid_url], options) do |result, identity_url, personal_data|
+      if result.successful?
+        authenticated(identity_url, personal_data)
+      else
+        login_failed(params.merge(:openid_url=>identity_url))
+      end
+    end
+  rescue StandardError => ex
+    logger.debug{ [ex.message, ex.backtrace].flatten.join("\n") }
+    login_failed(params)
   end
 
   def destroy
@@ -47,51 +63,43 @@ class SessionsController < ApplicationController
   end
 
   protected
-  # Track failed login attempts
-  def note_failed_signin(openid_url)
-    flash[:error] = _("Couldn't log you in as '#{openid_url}'")
-    logger.warn "Failed login for '#{openid_url}' from #{request.remote_ip} at #{Time.now.utc}"
+  def authenticated(identity_url, personal_data = {})
+    if user = find_user_by_identity_url(identity_url)
+      login_successfully(user, personal_data)
+      redirect_back_or_default(session[:return_to] || root_path)
+    else
+      signup_with_openid(identity_url, personal_data)
+      render :template=>"users/new"
+    end
   end
 
-  def login_with_openid(openid_url)
-    options = {
-      :optional => self.class.ax_attributes.values.flatten,
-      :method   => "get",
-    }
-    authenticate_with_open_id(openid_url, options) do |result, identity_url, personal_data|
-      if  result.successful?
-        # TODO クエリ最適化
-        if account = Account.find_by_identity_url(identity_url)
-          logged_in_successful(account.user, personal_data, session[:return_to] || root_path)
-        else
-          signup_with_openid(identity_url, personal_data)
-        end
-      else
-        login_failed(params.merge(:openid_url=>identity_url))
-      end
-    end
-  rescue StandardError => ex
-    logger.debug{ [ex.message, ex.backtrace].flatten.join("\n") }
-    login_failed(params.merge(:openid_url=>openid_url))
+  # ovverride
+  def find_user_by_identity_url(identity_url)
+    account = Account.find_by_identity_url(identity_url)
+    account && account.user
   end
 
-  def logged_in_successful(user, personal_data, redirect=root_path)
-    if SkipCollabo::OpFixation.sso_enabled?
-      data = self.class.translate_ax_response(personal_data)
-      user.update_attributes!(data.slice(:display_name))
-      flash[:notice] = _("Successfully synchronized your display name with OP's")
-    end
+  def sync_user_data(user, personal_data = {})
+    data = self.class.translate_ax_response(personal_data)
+    account.user.update_attributes!(data.slice(:display_name))
+    flash[:notice] = _("Successfully synchronized your display name with OP's")
+  end
+
+  def login_successfully(user, personal_data)
+    sync_user_data(user, personal_data) if SkipCollabo::OpFixation.sso_enabled?
+
     reset_session
     self.current_user = user
     new_cookie_flag = (params[:remember_me] == "1")
     handle_remember_cookie! new_cookie_flag
     flash[:notice] ||= _("Logged in successfully")
-    redirect_back_or_default(redirect)
   end
 
   # log login faulure. and re-render sessions/new
   def login_failed(assigns=params)
-    note_failed_signin(assigns[:openid_url])
+    flash[:error] = _("Couldn't log you in as '#{assigns[:openid_url]}'")
+    logger.warn "Failed login for '#{assigns[:openid_url]}' from #{request.remote_ip} at #{Time.now.utc}"
+
     @login       = assigns[:login]
     @openid_url  = assigns[:openid_url]
     @remember_me = assigns[:remember_me]
@@ -104,8 +112,6 @@ class SessionsController < ApplicationController
     data = self.class.translate_ax_response(ax_attributes)
     session[:user] = data if SkipCollabo::OpFixation.sso_enabled?
     @user = User.new(data)
-
-    render :template=>"users/new"
   end
 end
 
