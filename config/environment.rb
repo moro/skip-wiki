ecd19d8236fafa5521eed6b3958985dc14dbe920
moro <moronatural@gmail.com>
2009-04-02 09:43:51 +0900

    openskip-skip_embeddedのバージョンを0.9.0にした

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'moro-repim', :lib => 'repim', :source => 'http://gems.github.com/'
   config.gem 'moro-scope_do', :lib => 'scope_do', :version => '>=0.1.1', :source =>  'http://gems.github.com/'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :version=> '>=2.3.6', :source => 'http://gems.github.com/'
-  config.gem 'openskip-skip_embedded', :lib => 'skip_embedded', :version => '>=0.0.15', :source => 'http://gems.github.com'
+  config.gem 'openskip-skip_embedded', :lib => 'skip_embedded', :version => '>=0.9.0', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
